c3e6fe0018b5d3f4a9e74e46af566eaadba5140a
s190166 <s190166@student.pg.edu.pl>
2024-12-10 18:54:52 +0100

    Finanlna kod na uzupełnienie NA w bazie danych.

--- Zamiana Na.R
+++ Zamiana Na.R
@@ -1,5 +1,8 @@
-# Wczytaj pakiety (jeśli nie są zainstalowane, zainstaluj je najpierw)
+# Instalacja i załadowanie pakietów (jeśli nie są zainstalowane)
+if (!require("dplyr")) install.packages("dplyr")
+if (!require("mice")) install.packages("mice")
 library(dplyr)
+library(mice)
 
 # Wczytaj dane
 data <- supermarket
@@ -22,8 +25,20 @@ sum(is.na(data$City))
 
 # Zastąp wartości NA w 16 kolumnie wartościami z 9 kolumny
 data <- data %>%
-  mutate(across(16, ~ ifelse(is.na(.), data[[9]], .)))
+  mutate(`gross income` = ifelse(is.na(`gross income`), data[[9]], `gross income`))
 
-# Sprawdź, czy są jeszcze braki w 16 kolumnie
-sum(is.na(data[[16]]))
+# Sprawdź brakujące wartości w kolumnie 'gross income'
+sum(is.na(data$`gross income`))
 
+# Uzupełnianie brakujących wartości w całym zbiorze danych przy użyciu pakietu mice
+# (upewnij się, że dane wejściowe nie zawierają nienumerowalnych kolumn)
+imputed_data <- mice(data, m = 1, method = "pmm", maxit = 50, seed = 123)
+
+# Wybór uzupełnionego zestawu danych
+data <- complete(imputed_data, 1)
+
+# Sprawdzenie liczby brakujących wartości w całym zbiorze danych
+sum(is.na(data))
+
+# Eksport danych do pliku CSV
+write.csv(data, "supermarket_filled.csv", row.names = FALSE)
